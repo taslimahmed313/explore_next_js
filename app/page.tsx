a8cefb9c7603fde99faf052004fcfade8f866795
Taslim <taslimahmed313@gmil.com>
2023-05-10 06:12:00 +0600

    Data Fetching

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl">Helle World!</h1>
+      <h1 className="text-4xl">Home Page</h1>
       <Link className="text-3xl mt-8 inline-block" href="/about">
         Go To About Page
+      </Link>
+      <br />
+      <Link className="text-4xl mt-8 inline-block" href="/users">
+        Users
       </Link>
     </main>
   );
---
+++ lib/getAllUsers.tsx
@@ -0,0 +1,6 @@
+export default async function getAllUsers() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  if (!res.ok) throw new Error("Data Fetching Failed");
+  return res.json();
+}
